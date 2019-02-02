5c0d3ce4ac46a74197d8f53077a0b14017ce7d85
burakovas <burakovas@MacBook-Air-burakovas.local>
2019-02-02 19:10:47 +0300

    yii22_1

---
+++ frontend/views/task/view.php
@@ -0,0 +1,11 @@
+
+<a href="/task/one?id=<?= $model->id ?>">
+    
+<?= \app\widgets\MyWidget::widget([
+    'id' => $model->id,
+    'name' => $model->name,
+    'date' => $model->date,
+    'description' => $model->description,
+])?>
+
+</a>
